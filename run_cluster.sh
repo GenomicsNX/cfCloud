68a6e11c35d606b53f694484a86a17b5e165d2d4
NHLBI-BCB <NHLBI-BCB@users.noreply.github.com>
2020-06-09 07:15:32 -0400

    Update run_cluster.sh

--- run_cluster.sh
+++ run_cluster.sh
@@ -3,5 +3,17 @@
 
 resPath="results"
 
-snakemake -pr -k -d $resPath -j 120 --latency-wait 120 --cluster-config cluster_config.yaml --cluster "sbatch --cpus-per-task={cluster.cpus} \
---mem={cluster.mem} --time={cluster.time} -o {cluster.out} -e {cluster.out} -J {cluster.jobname}"
+snakemake -pr -k \
+   -d $resPath \
+   -j 120 \
+   --latency-wait 120 \
+   --cluster-config cluster_config.yaml \
+   --cluster "sbatch \
+   --cpus-per-task={cluster.cpus} \
+   --mem={cluster.mem} \
+   --time={cluster.time} \
+   -o {cluster.out} \
+   -e {cluster.out} \
+   -J {cluster.jobname}"
+   
+   
